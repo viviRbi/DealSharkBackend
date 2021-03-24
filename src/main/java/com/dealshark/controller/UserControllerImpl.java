b008dbc1a9b6025036c78bc6acb9d2cb5463fbab
Cooper Petty <copetty96@gmail.com>
2021-03-24 14:47:50 -0500

    made a small change to the user controller

--- src/main/java/com/dealshark/controller/UserControllerImpl.java
+++ src/main/java/com/dealshark/controller/UserControllerImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,8 @@ import com.dealshark.ajax.ClientMessage;
 import com.dealshark.model.User;
 import com.dealshark.service.UserService;
 
+@Controller("userController")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserControllerImpl implements UserController{
 	
 	@Autowired
