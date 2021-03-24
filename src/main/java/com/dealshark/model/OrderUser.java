c18fdd3b8cd6e7362d61577c75dbe11263bc1880
Cooper Petty <copetty96@gmail.com>
2021-03-23 20:51:29 -0500

    added a bunch of models need a little feedback before I go too far

---
+++ src/main/java/com/dealshark/model/OrderUser.java
@@ -0,0 +1,13 @@
+package com.dealshark.model;
+
+public class OrderUser {
+	
+	private int orderId; // this will be primary key I think
+	
+	private int userId; // think this is foreign key that goes to user model id
+	
+	private int totalPrice;
+	
+
+
+}
